16513dade5189d4593a8e328155538b4150ece31
Ronnie W <llqoli@gmail.com>
2022-06-08 19:47:19 +0800

    getJSON

---
+++ getJSON.sh
@@ -0,0 +1,2 @@
+wget "https://notion-api.splitbee.io/v1/table/ecf09f98966f4a90a17b7abac61fc128" -O sources.json
+wget "https://notion-api.splitbee.io/v1/table/efa6a396e3854a2592a88f787b9c4a19" -O appData.json
